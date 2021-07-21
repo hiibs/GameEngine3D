e6594c48fc90fc0b4ecf539f1e40c47bc2c0e21a
Iiro Leinonen <iippoleino@gmail.com>
2021-07-21 15:05:25 +0300

    Delete build

--- build/ext/assimp/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/dev/Leka3D/ext/assimp/contrib/zlib
-# Build directory: F:/dev/Leka3D/build/ext/assimp/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
